ce5d3def3d0ebdc68e9c8083da6cb2ca64dcdb14
Clauramarcelo <s77vqb9mcm@privaterelay.appleid.com>
2025-12-22 15:55:54 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 
 // --- Service Worker para GitHub Pages (sub-path /shotcrete-calc/) ---
 const REPO = '/shotcrete-calc';
-const CACHE_NAME = 'sc-v33'; // NUEVA versión
+const CACHE_NAME = 'sc-v34'; // NUEVA versión
 
 const ASSETS = [
   `${REPO}/`,
