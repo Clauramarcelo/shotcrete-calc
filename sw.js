80e011b610cc75c9dbcf8eb9e32576e1ade32935
Clauramarcelo <s77vqb9mcm@privaterelay.appleid.com>
2025-12-22 11:23:19 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 
 // --- Service Worker para GitHub Pages (sub-path /shotcrete-calc/) ---
 const REPO = '/shotcrete-calc';
-const CACHE_NAME = 'sc-v27'; // incrementa cuando cambies assets precacheados
+const CACHE_NAME = 'sc-v28'; // incrementa cuando cambies assets precacheados
 
 // Precache mínimo para funcionar offline
 const ASSETS = [
