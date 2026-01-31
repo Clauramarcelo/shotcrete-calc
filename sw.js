21bf5ac32f020c931df8e495b1888bfc3195d364
Clauramarcelo <s77vqb9mcm@privaterelay.appleid.com>
2026-01-30 21:51:43 -0500

    Update CACHE_NAME version in service worker

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // --- Service Worker para GitHub Pages (sub-path /shotcrete-calc/) ---
 const REPO = '/shotcrete-calc';
-const CACHE_NAME = 'sc-v43';
+const CACHE_NAME = 'sc-v46';
 
 const ASSETS = [
   `${REPO}/`,
